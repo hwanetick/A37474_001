478c864984271c793aa0cb48aa7d2676d90da3b6
hwanetick <hwanetick@etm-inc.com>
2018-03-07 16:18:09 -0800

    91-37474-001 Rev 2.0.0
    Shipping this version back to customer

--- A37474/FIRMWARE_VERSION.h
+++ A37474/FIRMWARE_VERSION.h
@@ -2,9 +2,9 @@
 #define __FIRMWARE_VERISON_H
 
 
-#define FIRMWARE_AGILE_REV     1
+#define FIRMWARE_AGILE_REV     2
 #define FIRMWARE_BRANCH        0
-#define FIRMWARE_MINOR_REV     1
+#define FIRMWARE_MINOR_REV     0
 
 
 #endif
